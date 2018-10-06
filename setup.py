aedc754695663e9f66a8dfde26def5fa478ebdf1
Steph Papanik <spapanik21@gmail.com>
2018-10-06 15:27:00 +0100

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-__version__ = "0.1.1"
 __author__ = "spapanik"
-
+__version__ = "0.1.1"
+__license__ = "GPLv2"
 PKG_NAME = "pickle_secure"
-PKG_URL = "https://github.com/{author}/{pkg_slug}".format(
-    author=__author__, pkg_slug=PKG_NAME.replace("_", "-")
-)
-
-
-def listify(filename):
-    with open(filename) as f:
-        return [line.strip() for line in f.readlines()]
+PKG_SLUG = PKG_NAME.replace("_", "-")
+PKG_URL = f"https://github.com/{__author__}/{PKG_SLUG}"
 
 
 def contents(filename):
@@ -23,18 +17,17 @@ setup(
     name=PKG_NAME,
     packages=[PKG_NAME],
     version=__version__,
+    license=__license__,
     description="Easily create encrypted pickle files",
-    license="GPLv2",
     long_description=contents("README.txt"),
     author=__author__,
     author_email="spapanik21@gmail.com",
     url=PKG_URL,
-    download_url="{pkg_url}/tarball/{ver}".format(
-        pkg_url=PKG_URL, ver=__version__
-    ),
-    keywords=["pickle", "AES", "secure storage"],
+    download_url=f"{PKG_URL}/tarball/{__version__}",
+    python_requires=">=3.6",
     install_requires=["pycrypto>=2.6.0,<2.7.0"],
     tests_require=["pytest>=3.0.0,<4.0.0"],
+    keywords=["pickle", "AES", "secure storage"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",

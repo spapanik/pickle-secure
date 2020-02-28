e446db7b2d2fc3e2d13151cd3f20a573f03e1227
Stephanos Kuma <spapanik21@gmail.com>
2020-02-28 10:09:51 +0000

    Turn key into a required keyword-only argument

--- src/pickle_secure/pickle_secure.py
+++ src/pickle_secure/pickle_secure.py
@@ -10,60 +10,60 @@ PicklingError = pickle.PicklingError
 UnpicklingError = pickle.UnpicklingError
 
 
-def dumps(obj, key, protocol=None, *, fix_imports=True):
+def dumps(obj, protocol=None, *, fix_imports=True, key):
     return utils.encrypt(obj, key, protocol, fix_imports)
 
 
-def dump(obj, file, key, protocol=None, *, fix_imports=True):
-    file.write(dumps(obj, key=key, protocol=protocol, fix_imports=fix_imports))
+def dump(obj, file, protocol=None, *, fix_imports=True, key):
+    file.write(dumps(obj, protocol=protocol, fix_imports=fix_imports, key=key))
 
 
-def loads(bytes_object, key, *, fix_imports=True, encoding="ASCII", errors="strict"):
+def loads(bytes_object, *, fix_imports=True, encoding="ASCII", errors="strict", key):
     return utils.decrypt(bytes_object, key, fix_imports, encoding, errors)
 
 
 def load(file, key, *, fix_imports=True, encoding="ASCII", errors="strict"):
     return loads(
-        file.read(), key, fix_imports=fix_imports, encoding=encoding, errors=errors,
+        file.read(), fix_imports=fix_imports, encoding=encoding, errors=errors, key=key
     )
 
 
 class Pickler(pickle.Pickler):
-    def __init__(self, file, key, protocol=None, *, fix_imports=True):
+    def __init__(self, file, protocol=None, *, fix_imports=True, key):
         super().__init__(file, protocol, fix_imports=fix_imports)
         self.__file = file
-        self.__key = key
         self.__protocol = protocol
         self.__fix_imports = fix_imports
+        self.__key = key
 
     def dump(self, obj):
         return dump(
             obj,
             self.__file,
-            self.__key,
             self.__protocol,
             fix_imports=self.__fix_imports,
+            key=self.__key,
         )
 
 
 class Unpickler(pickle.Unpickler):
     def __init__(
-        self, file, key, *, fix_imports=True, encoding="ASCII", errors="strict"
+        self, file, *, fix_imports=True, encoding="ASCII", errors="strict", key
     ):
         super().__init__(
             file, fix_imports=fix_imports, encoding=encoding, errors=errors
         )
         self.__file = file
-        self.__key = key
         self.__fix_imports = fix_imports
         self.__encoding = encoding
         self.__errors = errors
+        self.__key = key
 
     def load(self):
         return load(
             self.__file,
-            self.__key,
             fix_imports=self.__fix_imports,
             encoding=self.__encoding,
             errors=self.__errors,
+            key=self.__key,
         )
--- tests/pickle_secure/test_pickle_secure.py
+++ tests/pickle_secure/test_pickle_secure.py
@@ -11,25 +11,25 @@ def test_dump_load(tmpdir):
     secret = [123, "£¿éά", {(2, 3, 5): datetime.datetime.now()}]
     password = "strong password"
     with open(file, "wb") as f:
-        pickle_secure.dump(secret, f, password)
+        pickle_secure.dump(secret, f, key=password)
     with open(file, "rb") as f:
-        decrypted_data = pickle_secure.load(f, password)
+        decrypted_data = pickle_secure.load(f, key=password)
     assert secret == decrypted_data
 
 
 def test_dumps_loads():
     secret = [123, "£¿éά", {(2, 3, 5): datetime.datetime.now()}]
     password = "strong password"
-    encrypted_data = pickle_secure.dumps(secret, password)
-    assert secret == pickle_secure.loads(encrypted_data, password)
+    encrypted_data = pickle_secure.dumps(secret, key=password)
+    assert secret == pickle_secure.loads(encrypted_data, key=password)
 
 
 def test_dumps_loads_wrong_password():
     secret = [123, "£¿éά", {(2, 3, 5): datetime.datetime.now()}]
     password = "strong password"
     wrong_password = "weak password"
-    encrypted_data = pickle_secure.dumps(secret, password)
-    assert raises(InvalidToken, pickle_secure.loads, encrypted_data, wrong_password)
+    encrypted_data = pickle_secure.dumps(secret, key=password)
+    assert raises(InvalidToken, pickle_secure.loads, encrypted_data, key=wrong_password)
 
 
 def test_pickler_unpickler_classes(tmpdir):
@@ -37,9 +37,9 @@ def test_pickler_unpickler_classes(tmpdir):
     password = "strong password"
     secret = {123, "£¿éά", None}
     with open(file, "wb") as f:
-        pickler = pickle_secure.Pickler(f, password)
+        pickler = pickle_secure.Pickler(f, key=password)
         pickler.dump(secret)
     with open(file, "rb") as f:
-        unpickler = pickle_secure.Unpickler(f, password)
+        unpickler = pickle_secure.Unpickler(f, key=password)
         decrypted_data = unpickler.load()
     assert secret == decrypted_data
